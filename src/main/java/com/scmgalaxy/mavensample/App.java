7b44a2c44408ee72eeaafb1cf35cc4d3f4111a86
julius1975 <43832783+julius1975@users.noreply.github.com>
2019-05-30 16:11:22 -0400

    MPHAS-2 Fixed
    MPHAS-2 Fixed

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! MAH-3--d" );
+        System.out.println( "Hello World RefX! MPHAS-2--d" );
     }
 }
