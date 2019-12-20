7386eaeb1cbdabe2be93bc2c4c1ebf78b5908b47
julius1975 <43832783+julius1975@users.noreply.github.com>
2019-12-20 14:22:56 -0500

    IN-2 fixed

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! MPHAS-2--d" );
+        System.out.println( "Hello World RefX! IN-2-2--d" );
     }
 }
